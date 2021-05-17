f5cbf5aba1b14cd10c9896cf1356d0d88db2a8cb
StudeUni <48458762+StudeUni@users.noreply.github.com>
2021-05-17 17:30:19 +0100

    controller changes

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext _context)
+        {
+            this._context = _context;
+        }
+        public IActionResult Index()
+        {
+            _context.Items.FirstOrDefault();
+            return View("Item/Index");
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Add(item);
+            _context.SaveChanges();
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var item = _context.Items.Find(id);
+
+            if(item.Id == id)
+            {
+                _context.Items.Remove(item);
+                _context.SaveChanges();
+            }
+            return View("Index");
+        }
+    }
+}
